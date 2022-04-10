450842454745311de5930bc49857db7e8af3f6a4
MarcoAMtz99 <marcoamtz99@gmail.com>
2022-04-09 22:21:30 -0500

    Cambio en la opacidad fix del navbar mobile y cambio en las tarjetas del inicio

--- contacto.php
+++ contacto.php
@@ -61,7 +61,7 @@
           <span class="card-title"><strong>Ubicacion</strong></span>
           <br>
           <div class="map-responsive">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.533785621697!2d-99.18582873520045!3d19.43253964571834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8ac886eaaab%3A0x96e60ae5e3a9db70!2sMEXCEL!5e0!3m2!1ses-419!2smx!4v1649560764130!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.5067113067153!2d-99.18579928520039!3d19.433708245681252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8ac886cb0d7%3A0xf940a725b7a31393!2sTorcuato%20Tasso%20245-404a%2C%20Polanco%2C%20Polanco%20V%20Secc%2C%20Miguel%20Hidalgo%2C%2011550%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1649560860687!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         
         </div>
